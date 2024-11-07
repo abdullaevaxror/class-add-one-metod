7bc4eb072f95c886bf39e2d61ba33b6420829e0c
abdullaevaxror <abdullayevaxror122@gmail.com>
2024-11-07 16:49:24 +0500

    WorkDay.php add method

--- WorkDay.php
+++ WorkDay.php
@@ -2,34 +2,44 @@
 
 require 'DB.php';
 
-class WorkDay{
+class WorkDay
+{
     const REQUIRED_HOUR_DURATION = 8;
     public $pdo;
-    public function __construct () {
+    public function __construct()
+    {
         $db = new DB();
         $this->pdo = $db->pdo;
     }
-    public function store (string $name, string $arrived_at, string $leaved_at) {
+    public function store(string $name, string $arrived_at, string $leaved_at)
+    {
         // parametrdan arrived_at ni olib date object yasaymiz
-        $arrived_at = new DateTime($arrived_at);
-        $leaved_at = new DateTime($leaved_at);
-        $diff = $arrived_at->diff($leaved_at);
-        $hour = $diff->h;
-        $minute = $diff->i;
-        $second = $diff->s;
-        $total = ((self::REQUIRED_HOUR_DURATION * 3600) - (($hour * 3600) + ($minute * 60)));
+        $this->arrived_at = new DateTime($arrived_at);
+        $this->leaved_at = new DateTime($leaved_at);
+        $this->Workrequied();
+
         $query = "INSERT INTO work_time (name,arrived_at,leaved_at, required_of) 
                         VALUES (:name, :arrived_at, :leaved_at, :required_of)";
         $stmt = $this->pdo->prepare($query);
         $stmt->bindParam(':name', $name);
-        $stmt->bindValue(':arrived_at', $arrived_at->format('Y-m-d H:i'));
-        $stmt->bindValue(':leaved_at', $leaved_at->format('Y-m-d H:i'));
-        $stmt->bindParam(':required_of', $total);
+        $stmt->bindValue(':arrived_at', $this->arrived_at->format('Y-m-d H:i'));
+        $stmt->bindValue(':leaved_at', $this->leaved_at->format('Y-m-d H:i'));
+        $stmt->bindParam(':required_of', $this->total);
         $stmt->execute();
         header('Location: index.php');
         return;
     }
-    public function getWorDayList () {
+    public function Workrequied()
+    {
+        $diff = $this->arrived_at->diff($this->leaved_at);
+        $hour = $diff->h;
+        $minute = $diff->i;
+        $second = $diff->s;
+
+        $this->total = ((self::REQUIRED_HOUR_DURATION * 3600) - (($hour * 3600) + ($minute * 60)));
+    }
+    public function getWorDayList()
+    {
         $query = "SELECT * FROM work_time";
         $stmt = $this->pdo->query($query);
         return $stmt->fetchAll();
